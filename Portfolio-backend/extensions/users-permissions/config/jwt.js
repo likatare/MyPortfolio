d4186ba9dd19e2f0f95b9e90d28f3a047a5366f0
likatare <55482170+likatare@users.noreply.github.com>
2020-10-06 14:09:05 +0200

    First commit

---
+++ Portfolio-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '274f776c-3900-4917-87da-93ed072fc765'
+};
---
+++ Portfolio-frontend/src/constants/services.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    icon: <FaCode className="service-icon" />,
+    title: "internship system development",
+    text: `A company to grow in, be able to contribute and develop`,
+  },
+  {
+    id: 2,
+    icon: <FaSketch className="service-icon" />,
+    title: "web design",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+  {
+    id: 3,
+    icon: <FaAndroid className="service-icon" />,
+    title: "app design",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+]
