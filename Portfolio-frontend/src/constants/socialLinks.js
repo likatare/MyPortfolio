43c15ba85dba3ad28a4c45127c263d7c87b8b290
likatare <55482170+likatare@users.noreply.github.com>
2020-10-07 10:29:27 +0200

    Add about page

--- Portfolio-frontend/src/constants/socialLinks.js
+++ Portfolio-frontend/src/constants/socialLinks.js
@@ -11,12 +11,12 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/christoffer.hultgren.1",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/christoffer-hultgren/",
   },
   {
     id: 3,
@@ -31,7 +31,7 @@ const data = [
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://twitter.com/Hultgren_CH",
   },
 ]
 const links = data.map(link => {
